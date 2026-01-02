6b86202157acb708a9f452afc87fea36b62dd7de
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-02 13:50:35 -0500

    calculator launcher
    asks user for input of the first two numbers

---
+++ main.py
@@ -0,0 +1,6 @@
+#calculator launch 
+print("Thank you for using my calculator! I will try to update it with new features ")
+
+num1 = input("First number: ") #prompts user for the first number
+num2 = input ("Secound number: ") #prompts user for the sec number 
+
