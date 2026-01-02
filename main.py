7a6711d52817d9eda52e709c2b8df724dc24f30f
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-02 13:53:52 -0500

    addition logic

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ print("Thank you for using my calculator! I will try to update it with new featu
 num1 = input("First number: ") #prompts user for the first number
 num2 = input ("Secound number: ") #prompts user for the sec number 
 
+#number logic (magic)
+if num1 == "+":
+    print("sum:" + num1 + num2)
+
+    
