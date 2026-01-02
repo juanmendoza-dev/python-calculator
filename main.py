473ed92d1c653eaf767ecff90ba990c674fbc5f0
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-02 14:01:37 -0500

    added "-" logic

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 print("Thank you for using my calculator! I will try to update it with new features ")
 
 
-num1 = input("First number: ") #prompts user for the first number
-sign = input("sign (eg: +,-,*,/)") #prompts the user for the sign 
-num2 = input ("Secound number: ") #prompts user for the sec number 
+num1 = input (int("First number: ")) #prompts user for the first number
+sign = input ("sign (eg: +,-,*,/)") #prompts the user for the sign 
+num2 = input (int("Secound number: ")) #prompts user for the sec number 
 
 #number logic (magic)
-if num1 == "+":
+if sign == "+":
     print("sum:" + num1 + num2)
-elif ()
-    print("")
+elif ("-"):
+    print("diffrence:" + num1 - num2)
+
     
