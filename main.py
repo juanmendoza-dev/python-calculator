6075ca96bc39f95a75a4d8d79cc05ad728139cb7
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-02 14:06:03 -0500

    Completed Logic + Added Error

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ print("Thank you for using my calculator! I will try to update it with new featu
 
 num1 = input (int("First number: ")) #prompts user for the first number
 sign = input ("sign (eg: +,-,*,/)") #prompts the user for the sign 
-num2 = input (int("Secound number: ")) #prompts user for the sec number 
+num2 = input (int("Second number: ")) #prompts user for the sec number 
 
 #number logic (magic)
 if sign == "+":
-    print("sum:" + num1 + num2)
+    print("sum: " + num1 + num2)
 elif (sign == "-"):
-    print("diffrence:" + num1 - num2)
-elif (sign == "*")
+    print("difference: " + num1 - num2)
+elif (sign == "*"):
+    print("product: " + num1 * num2)
+elif(sign == "/"):
+    print("quotient: "+ num1 / num2)
+else:
+    print("invalid! Please try again!")
+    
 
     
