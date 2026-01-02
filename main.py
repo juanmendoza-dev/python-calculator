1d842e14217ef51d408b51ac82f010b6668db37c
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-02 13:57:02 -0500

    updated the launcher

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #calculator launch 
 print("Thank you for using my calculator! I will try to update it with new features ")
 
+
 num1 = input("First number: ") #prompts user for the first number
+sign = input("sign (eg: +,-,*,/)") #prompts the user for the sign 
 num2 = input ("Secound number: ") #prompts user for the sec number 
 
 #number logic (magic)
 if num1 == "+":
     print("sum:" + num1 + num2)
-
+elif ()
+    print("")
     
