20a9706bbc6275d0b93255cd3fb9d65bb0cedd66
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-02 22:30:54 -0500

    fixed all of the number logic
    I didn't know that string---> int, was no bueno so I fixed that.
    
    Stable From this update (excluding colors)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from colorama import Fore 
+
 #calculator launch 
 print("Thank you for using my calculator! I will try to update it with new features ")
 
 
-num1 = input (int("First number: ")) #prompts user for the first number
-sign = input ("sign (eg: +,-,*,/)") #prompts the user for the sign 
-num2 = input (int("Second number: ")) #prompts user for the sec number 
+num1 =  input("First number: ") #prompts user for the first number
+sign = input("sign (eg: +,-,*,/)") #prompts the user for the sign 
+num2 = input("Second number: ") #prompts user for the sec number 
 
 #number logic (magic)
 if sign == "+":
@@ -17,6 +19,9 @@ elif(sign == "/"):
     print("quotient: "+ num1 / num2)
 else:
     print("invalid! Please try again!")
-    
+
+
+    #TODO: add colors
+    #TODO: Get the launcher working
 
     
