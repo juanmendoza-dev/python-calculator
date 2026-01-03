7d6a94f30a8d48f269a825c906faf67f76e8e62f
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-03 01:18:44 -0500

    Completed Calculator Logic
    Everything on the logic side works as it should be intended. Also added some planned updates to be more organized

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from colorama import Fore
 print(Fore.LIGHTMAGENTA_EX + "Thank you for using my calculator! I will try to update it with new features ")
 
 
-num1 =  input("First number: ") #prompts user for the first number
-sign = input("sign (eg: +,-,*,/)") #prompts the user for the sign 
-num2 = input("Second number: ") #prompts user for the sec number 
+num1 =  int(input(Fore.LIGHTBLUE_EX + "First number: ")) #prompts user for the first number
+sign = input(Fore.LIGHTYELLOW_EX + "sign (eg: + | - | * | /): ") #prompts the user for the sign 
+num2 = int(input(Fore.LIGHTBLUE_EX +"Second number: ")) #prompts user for the sec number 
 
 #number logic (magic)
 if sign == "+":
-    print("sum: " + num1 + num2)
+    print(f"{Fore.LIGHTGREEN_EX}Sum: {num1 + num2}")
 elif (sign == "-"):
-    print("difference: " + num1 - num2)
+    print(f"{Fore.LIGHTGREEN_EX}Difference: {num1 - num2}")
 elif (sign == "*"):
-    print("product: " + num1 * num2)
+    print(f"{Fore.LIGHTGREEN_EX}Product: {num1 * num2}")
 elif(sign == "/"):
-    print("quotient: "+ num1 / num2)
+    print(f"{Fore.LIGHTGREEN_EX}Quotient: {num1 / num2}")
 else:
-    print("invalid! Please try again!")
+    print(Fore.LIGHTRED_EX + "invalid! Please try again!")
 
 
-    #TODO: add colors
-    #TODO: Correctly adjust colors!
+    #planned updates!
+    #TODO: add auto refresh (after a 2-3 secs make the page reset back the first prompt)
+    #TODO: add memory system! (make the user be able to go back to previous outputs)
+    #TODO: add settings launcher for the user to able adjust things like: auto refresh time, if they want sys memory or na.
+    
 
     
