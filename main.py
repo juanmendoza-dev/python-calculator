ada89337664dd0e1b2c552cb9d897b8aae74fe46
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-04 18:59:56 -0500

    Added UI optimization + Auto Refresh
    Made the UI more clean for new outputs. Also made the calculator keep looping after a output is generated!

--- main.py
+++ main.py
@@ -2,32 +2,40 @@ from colorama import Fore
 
 #calculator launch 
 print(Fore.LIGHTMAGENTA_EX + "Thank you for using my calculator! I will try to update it with new features!")
-print(Fore.LIGHTMAGENTA_EX) + 'Feel free to press "P" to quit!' 
+print(Fore.LIGHTMAGENTA_EX + 'Feel free to press "E" to exit!')
 
 
 def mainLauncher():
-    num1 =  int(input(Fore.LIGHTBLUE_EX + "First number: ")) #prompts user for the first number
-    sign = input(Fore.LIGHTYELLOW_EX + "sign (eg: + | - | * | /): ") #prompts the user for the sign 
-    num2 = int(input(Fore.LIGHTBLUE_EX +"Second number: ")) #prompts user for the sec number 
-
+    num1 = 0 #placeholder 
+    while not num1 == "e":
+        num1 =  int(input(Fore.LIGHTBLUE_EX + "First number: ")) #prompts user for the first number
+        sign = input(Fore.LIGHTYELLOW_EX + "sign (eg: + | - | * | /): ") #prompts the user for the sign 
+        num2 = int(input(Fore.LIGHTBLUE_EX +"Second number: ")) #prompts user for the sec number
+     
+    
 #number logic (magic)
-    if sign == "+":
-        print(f"{Fore.LIGHTGREEN_EX}Sum: {num1 + num2}")
-    elif (sign == "-"):
-        print(f"{Fore.LIGHTGREEN_EX}Difference: {num1 - num2}")
-    elif (sign == "*"):
-        print(f"{Fore.LIGHTGREEN_EX}Product: {num1 * num2}")
-    elif(sign == "/"):
-        print(f"{Fore.LIGHTGREEN_EX}Quotient: {num1 / num2}")
-    else:
-        print(Fore.LIGHTRED_EX + "invalid! Please try again!")   
+        if sign == "+":
+            print(f"{Fore.LIGHTGREEN_EX}Sum: {num1 + num2}")
+            print("-------------------")
+        elif (sign == "-"):
+            print(f"{Fore.LIGHTGREEN_EX}Difference: {num1 - num2}")
+            print("-------------------")
+        elif (sign == "*"):
+            print(f"{Fore.LIGHTGREEN_EX}Product: {num1 * num2}")
+            print("-------------------")
+        elif(sign == "/"):
+            print(f"{Fore.LIGHTGREEN_EX}Quotient: {num1 / num2}")
+            print("-------------------")
+        else:
+            print(Fore.LIGHTRED_EX + "invalid! Please try again!")   
 
 #launcher 
-mainLauncher()
+mainLauncher() #launches calculator 
+
 
 
     #planned updates!
-    #TODO: add auto refresh (after a 2-3 secs make the page reset back the first prompt)
+    #TODO: make a system where the user can exit (quit)
     #TODO: add memory system! (make the user be able to go back to previous outputs)
     #TODO: add settings launcher for the user to able adjust things like: auto refresh time, if they want sys memory or na.
     #TODO: make the calculator launch on a cmd/terminal window 
