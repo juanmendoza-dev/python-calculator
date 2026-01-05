cecda5ddc499a8f1a8bdc5537d9e5cac915aaba5
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-05 16:54:48 -0500

    Added exit feature
    now if the user wants to leave the calculator they can by pressing "e"

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ print(Fore.LIGHTMAGENTA_EX + 'Feel free to press "E" to exit!')
 
 
 def mainLauncher():
-    num1 = "placeholder"  
-    while not num1 == "e":
-        num1 =  int(input(Fore.LIGHTBLUE_EX + "First number: ")) #prompts user for the first number
+    while True:
+        num1 =  input(Fore.LIGHTBLUE_EX + "First number: ") #prompts user for the first number
+
+        if num1 == "e": 
+            break
+        
+        num1 = int(num1)
         sign = input(Fore.LIGHTYELLOW_EX + "sign (+ | - | * | /): ") #prompts the user for the sign 
         num2 = int(input(Fore.LIGHTBLUE_EX +"Second number: ")) #prompts user for the sec number
      
-    
 
 
 #number logic (magic)
@@ -29,15 +32,16 @@ def mainLauncher():
             print(f"{Fore.LIGHTGREEN_EX}Quotient: {num1 / num2}")
             print("-------------------")
         else:
-            print(Fore.LIGHTRED_EX + "invalid! Please try again!")   
+            print(Fore.LIGHTRED_EX + "invalid! Please try again!")  
+            print("-------------------")
 
 #launcher 
 mainLauncher() #launches calculator 
 print(Fore.LIGHTMAGENTA_EX + "thank you for using my awesome calculator please star on github!")
 
-
     #planned updates!
     #TODO: make a system where the user can exit (quit)
+    #TODO: fix error: user puts space instead of number
     #TODO: add settings launcher for the user to able adjust things like: auto refresh time, if they want sys memory or na.
     #TODO: add performance mode (delayed refresh, anti spam)
     #TODO: make the calculator launch on a cmd/terminal window 
