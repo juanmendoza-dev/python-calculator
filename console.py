7f6f353e2a35f54e249097109e584ea75d888bfe
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-09 13:43:20 -0500

    Added Console Logic

--- console.py
+++ console.py
@@ -1,20 +1,39 @@
 import time
 from colorama import Fore
 
-
-def cal_console():
+#console launcher
+def console_launcher():
     print(Fore.LIGHTYELLOW_EX + "loading console...") #2 sec load time for console
     time.sleep(2)
     print(Fore.LIGHTGREEN_EX + "Console Loaded!")
-    while True:
-        console = input(Fore.LIGHTYELLOW_EX + "Console: ")
-        if console == ("restart"):
+
+    #restart console function
+def console_restart():
             print(Fore.LIGHTYELLOW_EX + "Restarting...") #restarts console
-            mainLauncher()
-            break
+            print(Fore.LIGHTYELLOW_EX + "-------------------")
+            time.sleep(2)
+            print(Fore.LIGHTGREEN_EX +"calcualtor sucessfulley loaded!")
+            return
+    
+while True:
+    console = input(Fore.LIGHTYELLOW_EX + "Console: ")
+    if console == ("restart"):
+           console_restart()
+    elif (console == "back"):
+           print("still working on this!")
+    else:
+           print("invalid input!")
+           
+           
+    
+    
 
 
-#planned updates for console
-#TODO: Add settings bar 
-#TODO: Performance mode 
-#TODO: quick maths 
+
+
+
+            
+
+
+#planned updates
+#TODO: add the restart function
