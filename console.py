1053dd58c98487e73a3452c67921e95f76d639f1
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-08 21:58:00 -0500

    Added a Planned updates to the console

--- console.py
+++ console.py
@@ -3,14 +3,18 @@ from colorama import Fore
 
 
 def cal_console():
-    print(Fore.LIGHTYELLOW_EX + "loading console...")
+    print(Fore.LIGHTYELLOW_EX + "loading console...") #2 sec load time for console
     time.sleep(2)
     print(Fore.LIGHTGREEN_EX + "Console Loaded!")
     while True:
         console = input(Fore.LIGHTYELLOW_EX + "Console: ")
         if console == ("restart"):
-            print(Fore.LIGHTYELLOW_EX + "Restarting...")
+            print(Fore.LIGHTYELLOW_EX + "Restarting...") #restarts console
             mainLauncher()
             break
 
 
+#planned updates for console
+#TODO: Add settings bar 
+#TODO: Performance mode 
+#TODO: quick maths 
