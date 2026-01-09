bf4b6ce53eae2743ca27975e2274b897cf70202a
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-09 13:56:00 -0500

    Fixed all of main.py bugs + console.py bugs
    Fixed all the logic in the console (undefined functions stopping main.py from running and make the console override and run first)

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ def console_launcher():
     print(Fore.LIGHTYELLOW_EX + "loading console...") #2 sec load time for console
     time.sleep(2)
     print(Fore.LIGHTGREEN_EX + "Console Loaded!")
+    console_logic()
 
     #restart console function
 def console_restart():
@@ -14,19 +15,20 @@ def console_restart():
             time.sleep(2)
             print(Fore.LIGHTGREEN_EX +"calcualtor sucessfulley loaded!")
             return
-    
-while True:
-    console = input(Fore.LIGHTYELLOW_EX + "Console: ")
-    if console == ("restart"):
-           console_restart()
-    elif (console == "back"):
-           print(Fore.LIGHTYELLOW_EX +"returing to calcualtor...")
-           time.sleep(1)
-           print(Fore.LIGHTGREEN_EX + "success!!!!")
-           break
+  
+def console_logic():
+        while True:
+            console = input(Fore.LIGHTYELLOW_EX + "Console: ")
+            if console == ("restart"):
+                console_restart()
+            elif (console == "back"):
+                print(Fore.LIGHTYELLOW_EX +"returing to calcualtor...")
+                time.sleep(1)
+                print(Fore.LIGHTGREEN_EX + "success!!!!")
+                break
 
-    else:
-           print(Fore.LIGHTRED_EX + "invalid input!")   
+            else:
+                print(Fore.LIGHTRED_EX + "invalid input!")   
     
     
 
