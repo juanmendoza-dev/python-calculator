e54a3e231f0ddb4096752895d9c04ecd41a2fa85
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-16 13:52:23 -0500

    Added the backend setup

---
+++ backend.py
@@ -0,0 +1,18 @@
+#BETA VERSION RIGHTNOW! 
+# sorry if its a little bit glitchy 
+
+import time 
+from colorama import Fore
+import main
+
+
+def backend_console():
+    opened_console = "opened console"
+    closed_console = "closed console"
+    restarted_console = "restarted console"
+    loaded_help = "loaded help"
+    loaded_overlock = "loaded overclock"
+    program_killed = "program killed"
+    launcher = "launched calcualtor"
+
+
